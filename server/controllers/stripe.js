ec907401b31b47c3b35c009203299f53558c7e47
David Vu <davidvu7887@gmail.com>
2023-08-11 20:48:58 -0700

    Created stripe.js file

--- server/controllers/stripe.js
+++ server/controllers/stripe.js
@@ -1,31 +1,22 @@
 import stripe from "stripe";
 stripe(process.env.STRIPE_PRIVATE_KEY);
 
-const subscriptionItems = new Map([
-    [1, { priceInCents: 399, name: "Platina Subscription"}]
-])
+const priceID = "price_1NYKgkGJReRCK7tcj2MMju0W"
 
 export const createCheckoutSession = async (req, res) => {
     try {
-        const session = await stripe.Checkout.SessionsResource.create({
-            payment_method_types: ['card'],
+        const session = await stripe.checkout.sessions.create({
             mode: "subscription",
-            line_items: req.body.items.map(subscription => {
-                const subscriptionItem = subscriptionItems.get(subscription.id);
-                return {
-                    price_data: {
-                        currency: "usd",
-                        product_data: {
-                            name: subscriptionItem.name
-                        },
-                        unit_amount: subscriptionItem.priceInCents
-                    },
-                    quantity: subscription.quanity
+            line_items: [
+                {
+                    price: priceID,
+                    quantity: 1
                 }
-            }),
+            ],
             success_url: "",
             cancel_url: ""
         })
+        
         res.json({ url: session.url })
     } catch (err) {
         res.status(500).json({ error: err.message });
